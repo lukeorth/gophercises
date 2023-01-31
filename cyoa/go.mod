314b80a7264997267ab0f68b489d427667cac24d
Luke C. Orth <lorth001@gmail.com>
2023-01-30 20:54:37 -0600

    Adding cyoa

---
+++ cyoa/go.mod
@@ -0,0 +1,3 @@
+module github.com/lukeorth/gophercises/cyoa
+
+go 1.18
---
+++ cyoa/story.go
@@ -0,0 +1,48 @@
+package cyoa
+
+import (
+	"encoding/json"
+	"io"
+)
+
+var defaultHandlerTmpl = `
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Choose Your Own Adventure</title>
+    </head>
+    <body>
+        <h1>{{.Title}}</h1>
+        {{range .Paragraphs}}
+            <p>{{.}}</p>
+        {{end}}
+        <ul>
+        {{range .Options}}
+            <li><a href="/{{.Chapter}}">{{.Text}}</a></li>
+        {{end}}
+        </ul>
+    </body>
+</html>`
+
+func JsonStory(r io.Reader) (Story, error) {
+    d := json.NewDecoder(r)
+    var story Story
+    if err := d.Decode(&story); err != nil {
+        return nil, err
+    }
+    return story, nil
+}
+
+type Story map[string]Chapter
+
+type Chapter struct {
+    Title      string   `json:"title"`
+    Paragraphs []string `json:"story"`
+    Options    []Option `json:"options"` 
+}
+
+type Option struct {
+    Text string `json:"text"`
+    Arc  string `json:"arc"`
+}
