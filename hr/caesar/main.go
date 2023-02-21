7c3d60324f55af4f2f981611ae2a3c288081f468
Luke C. Orth <lorth001@gmail.com>
2023-02-21 14:57:07 -0600

    Cleaned up the formula for calculating rotation

--- hr/caesar/main.go
+++ hr/caesar/main.go
@@ -12,18 +12,18 @@ func main() {
     s := bufio.NewScanner(os.Stdin)
     s.Scan()
     input := s.Text()
-    k := 27
+    k := 3
 
     var cipher string
 
     for _, ch := range input {
         if unicode.IsUpper(ch) {
-            rot := 65 + ((k - (90 - int(ch) + 1) + 26) % 26)
+            rot := 65 + (int(ch) - 65 + k) % 26
             cipher += string(rot)
             continue
         }
         if unicode.IsLower(ch) {
-            rot := 97 + ((k - (122 - int(ch) + 1) + 26) % 26)
+            rot := 97 + (int(ch) - 97 + k) % 26
             cipher += string(rot)
             continue
         }
